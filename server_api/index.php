08f19644e1704a6e504b7b00036823a547d61882
rajeshstan <sundar.goals@gmail.com>
2015-05-07 14:44:07 -0400

    fixed access_code to print returning access_code works with post method returning access_code row works with get method

--- server_api/index.php
+++ server_api/index.php
@@ -6,6 +6,7 @@ include 'db.php';
 $app = new \Slim\Slim();
 $app->get('/users','getUsers');
 $app->post('/users','postUsers');
+$app->get('/users/:access_code','accessCodeData');
 $app->get('/updates','getUserUpdates');
 $app->get('/challenges','getChallenges');
 $app->get('/dummy', 'dummyTable');
@@ -20,12 +21,9 @@ $app->run();
 
 
 function getUsers() {
-$sql = "CREATE TABLE `users` (
-`user_id` int(11) AUTO_INCREMENT, `wins` int(100), `class` char(50),
-PRIMARY KEY (`user_id`)
-);";
-$sql = "INSERT INTO `users` (`user_id`, `class`,`wins`) VALUES (7, `bond`,50);";
-//$sql = "SELECT user_id FROM `users`;";
+
+//$sql = "INSERT INTO `users` (`user_id`, `class`,`wins`) VALUES (7, `bond`,50);";
+$sql = "SELECT user_id,access_code FROM `users`;";
 
 try {
 $app = \Slim\Slim::getInstance();
@@ -40,41 +38,68 @@ echo "test table " . json_encode($users);
 echo $sql . '{"error":{"text":'. $e->getMessage() .'}}';
 }
 }
-function postUsers() {
 
-$value=rand(0,9999);
-echo $value;
+function postUsers(){
+
+   // $value=rand(0,9999);
+   // echo $value;
 
 
 // echo $sql
 //$sql = "INSERT INTO `users` (`user_id`, `class`,`wins`) VALUES (7, `bond`,50, );";
 //$sql = "SELECT user_id FROM `users`;";
 
+    try {
+        $app = \Slim\Slim::getInstance();
+        $db = getDB();
+
+
+            do{
+                $value=rand(0,9999);
+               // echo $value;
+                $checkvalue = "SELECT user_id FROM users WHERE access_code = $value";
+
+                $stmt = $db->prepare($checkvalue);
+                $stmt->execute();
+            }while($stmt->rowCount()>0);
+
+
+            $sql = "INSERT INTO `users` (`user_id`, `access_code`, `class`,`wins`) VALUES (DEFAULT, $value,NULL,0);";
+            $stmt = $db->prepare($sql);
+            $stmt->execute();
+            $users = $stmt->fetchAll(PDO::FETCH_OBJ);
+            $db = null;
+            $app->response->setStatus(200);
+
+
+        } catch(PDOException $e) {
+            echo ' ';
+        }
+        echo $value;
+    }
+
+function accessCodeData($access_code) {
+
+$sql = "SELECT * FROM `users` WHERE access_code=:access_code;";
+//$sql = "INSERT INTO `users` (`user_id`, `class`,`wins`) VALUES (7, `bond`,50);";
+//$sql = "SELECT user_id FROM `users`;";
+
 try {
 $app = \Slim\Slim::getInstance();
 $db = getDB();
-
-
-    do{
-        $value=rand(0,9999);
-        $checkvalue = "SELECT user_id FROM users WHERE access_code = $value";
-
-        $stmt = $db->prepare($checkvalue);
-        $stmt->execute();
-      }while($stmt->rowCount()>0);
-
-
-$sql = "INSERT INTO `users` (`user_id`, `access_code`, `class`,`wins`) VALUES (DEFAULT, $value,NULL,0);";
- $stmt = $db->prepare($sql);
- $stmt->execute();
+$stmt = $db->prepare($sql);
+$stmt->bindParam("access_code", $access_code, PDO::PARAM_INT);
+$stmt->execute();
 $users = $stmt->fetchAll(PDO::FETCH_OBJ);
+//print_r($users);
 $db = null;
 $app->response->setStatus(200);
-echo $value;
+echo "test table " . json_encode($users);
 } catch(PDOException $e) {
-//echo $sql . '{"error":{"text":'. $e->getMessage() .'}}';
+echo $sql . '{"error":{"text":'. $e->getMessage() .'}}';
 }
 }
+
 function getChallenges() {
 $sql = "CREATE TABLE `challenges` (
 `challenge_id` int(11) AUTO_INCREMENT, `user_id` int(11),`user1` char(10), `user2` char(10), `button_chosen` char(100), `time` char(50),
